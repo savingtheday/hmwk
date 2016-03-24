2442b96cef4ea019cd4a1346402cb82922bc4490
Kris <weldon.kris@gmail.com>
2016-03-24 17:47:41 -0400

    updated

--- cookies/spript.js
+++ cookies/spript.js
@@ -1,31 +1,24 @@
-  Cookies.set("hello", "cookies");
+  $(document).ready(function(){
 
-
-$(document).ready(function(){
-  alert('setting');
-  Cookies("hello", "cookies");
     function set_cookies(){
+
       if (Cookies($('#cookie1').text())) {
-        $('#cookie1-number').text(Cookies($('#cookie1').text()));
+        $('#cookie1-number').text(Cookies($('#cookie1').text())); //make the text reflect the number of clicks/cookies
         $('#cookie2-number').text(Cookies($('#cookie2').text()));
         $('#cookie3-number').text(Cookies($('#cookie3').text()));
       } else {
-        Cookies.set($('#cookie1').text(), $('#cookie1-number').text());
-        Cookies.set($('#cookie2').text(), $('#cookie2-number').text());
-        Cookies.set($('#cookie3').text(), $('#cookie3-number').text());
+        Cookies.set($('#cookie1').text(), 0); //has to be initialized with something here or in html between span tags
+        Cookies.set($('#cookie2').text(), 0);
+        Cookies.set($('#cookie3').text(), 0);
       };
 
     }
     $('.cookie').click(function(){
       var cookie = $('#'+$(this).attr('id')).text();
-      Cookies.set(cookie, parseInt(Cookies(cookie)) + 1);
+      Cookies.set(cookie, parseInt(Cookies(cookie)) + 1); //make integer, add 1
       var num = Cookies(cookie);
-      $('#' + $(this).attr('id') + '-number').text(num);
-      $('#ate').text("You ate a " + cookie + " cookie.");
+      $('#' + $(this).attr('id') + '-number').text(num); //-number adds the id to unique classes in the html
     });
-      set_cookies();
+    set_cookies();
 
-
-
-};
-});
+  });
